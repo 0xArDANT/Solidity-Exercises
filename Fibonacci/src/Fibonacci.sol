b016a037a3546027c87c9cca99871fee8012a0a7
0xArDANT <aroldtouko94@gmail.com>
2024-08-29 12:49:36 +0100

    Completed EverythingWorks, Exponent, Fibonacci

--- Fibonacci/src/Fibonacci.sol
+++ Fibonacci/src/Fibonacci.sol
@@ -11,7 +11,34 @@ contract Fibonacci {
         calling fibonacci(6) would return 8, because the 6th Fibonacci number is 8.
     */
 
-    function fibonacci(uint256 _position) public view returns (uint256) {
-        // your code here
+
+    // Without reccursion (15429 gas)
+    function fibonacci2(uint256 _position) public pure returns (uint256) {
+        if(_position == 0) return 0;
+        if(_position == 1) return 1;
+         else {
+            uint256 fibonacciN0 = 0;
+            uint256 fibonacciN1 = 1;
+            uint256 fibonacciN2;
+
+            for(uint256 i=2; i<= _position; i++) {
+                fibonacciN2 = fibonacciN0 + fibonacciN1;
+                if(i != _position) {
+                    fibonacciN0 = fibonacciN1;
+                    fibonacciN1 = fibonacciN2;
+                }
+            }
+            return fibonacciN2;
+        }
+    }
+
+    // With reccursion : 4248958 gas
+
+    function fibonacci(uint256 _position) public pure returns (uint256) {
+        if(_position == 0) return 0;
+        if(_position == 1) return 1;
+        else {
+            return fibonacci(_position - 1) + fibonacci(_position - 2);
+        }
     }
 }
